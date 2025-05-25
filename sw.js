56398b186de2430b3c95ef7e8e46012aa90bb49d
PieterHimpe <himpe_pieter@hotmail.com>
2025-05-25 21:46:15 +0200

    Update sw.js
    meer bewaren

--- sw.js
+++ sw.js
@@ -1,21 +1,40 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("block-party-cache").then(cache =>
-      cache.addAll([
+const CACHE_NAME = "block-party-cache-v2";
+
+self.addEventListener("install", event => {
+  self.skipWaiting(); // Activeer meteen
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
         "./",
         "./index.html",
         "./manifest.json",
         "./sw.js",
         "./icon-192.png",
         "./icon-512.png"
-        // Voeg hier andere bestanden toe zoals JS, CSS, afbeeldingen, geluiden...
-      ])
+        // voeg hier je andere bestanden toe: JS, CSS, afbeeldingen...
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+  // Verwijder oude caches
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
     )
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request) // 🔄 Altijd eerst netwerk proberen
+      .then(response => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      })
+      .catch(() => caches.match(event.request)) // Als offline: gebruik cache
   );
 });
